897a1dd41a66e66ed8c4372892bf11e866bedaa4
morkeno <ole_magnus_morken@outlook.com>
2025-02-26 12:09:28 +0100

    FEATURE: basic working template for minimal setup of function app

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -32,13 +32,6 @@ resource "azurerm_service_plan" "function_service_plan" {
   }
 }
 
-# resource "azurerm_application_insights" "application_insights" {
-#   name                = "${var.project}-${var.environment}-application-insights"
-#   location            = var.location
-#   resource_group_name = azurerm_resource_group.function_rg.name
-#   application_type    = "Node.JS"
-# }
-
 resource "azurerm_linux_function_app" "function_app" {
   name                       = "${var.project}-${var.environment}-function-app"
   location                   = azurerm_resource_group.function_rg.location
@@ -47,12 +40,6 @@ resource "azurerm_linux_function_app" "function_app" {
   storage_account_name       = azurerm_storage_account.function_storage_account.name
   storage_account_access_key = azurerm_storage_account.function_storage_account.primary_access_key
 
-#   app_settings = {
-#     "WEBSITE_RUN_FROM_PACKAGE"       = "",
-#     "FUNCTIONS_WORKER_RUNTIME"       = "node",
-#     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key,
-#   }
-#
   lifecycle {
     ignore_changes = [
       app_settings["WEBSITE_RUN_FROM_PACKAGE"],
@@ -60,16 +47,18 @@ resource "azurerm_linux_function_app" "function_app" {
   }
 
   app_settings = {
-    FUNCTIONS_WORKER_RUNTIME    = "node"
-    AzureWebJobsStorage         = azurerm_storage_account.function_storage_account.primary_blob_connection_string
-    WEBSITE_RUN_FROM_PACKAGE    = "1"
+    FUNCTIONS_WORKER_RUNTIME = "node"
+    AzureWebJobsStorage      = azurerm_storage_account.function_storage_account.primary_blob_connection_string
+    WEBSITE_RUN_FROM_PACKAGE = "1"
   }
 
-
-  site_config {}
+  site_config {
+    application_stack {
+      node_version = "20"
+    }
+  }
 
   tags = {
     environment = var.environment
   }
 }
-
