464e6ad29dca99f2a0b0ba4e75753eefeb6881dc
morkeno <ole_magnus_morken@outlook.com>
2025-02-26 09:37:46 +0100

    FEATURE: start work on AI prompting

--- src/functions/FnTilbudMagi.ts
+++ src/functions/FnTilbudMagi.ts
@@ -1,6 +1,7 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import "../app/OpenAIClient"
 
-export async function MyHttpTrigger(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+export async function FnTilbudMagi(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
@@ -8,8 +9,8 @@ export async function MyHttpTrigger(request: HttpRequest, context: InvocationCon
     return { body: `Hello, ${name}!` };
 };
 
-app.http('MyHttpTrigger', {
+app.http('tilbudmagi', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: MyHttpTrigger
+    handler: FnTilbudMagi
 });
