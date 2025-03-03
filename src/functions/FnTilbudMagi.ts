ed1fb2bef6c8fe7c19fd86e5d7a1d1193e595d02
morkeno <ole_magnus_morken@outlook.com>
2025-03-03 09:50:43 +0100

    FEATURE: functioning hello-world function

--- src/functions/FnTilbudMagi.ts
+++ src/functions/FnTilbudMagi.ts
@@ -1,7 +1,6 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import "../app/OpenAIClient"
 
-export async function FnTilbudMagi(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+export async function HelloWorldFunction(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
@@ -9,8 +8,8 @@ export async function FnTilbudMagi(request: HttpRequest, context: InvocationCont
     return { body: `Hello, ${name}!` };
 };
 
-app.http('tilbudmagi', {
+app.http('HelloWorldFunction', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: FnTilbudMagi
+    handler: HelloWorldFunction
 });
