33021e7b3252094a433cde5aba66b6e9a4d10f85
morkeno <ole_magnus_morken@outlook.com>
2025-03-04 14:51:41 +0100

    FEATURE: parse incoming data

--- src/functions/FnTilbudMagi.ts
+++ src/functions/FnTilbudMagi.ts
@@ -1,8 +1,24 @@
 import {app, HttpRequest, HttpResponseInit, InvocationContext} from "@azure/functions";
 import {PromptAI} from '../app/OpenAIClient'
+import {Readable} from "node:stream";
+
+async function streamToString(stream: Readable): Promise<string> {
+    return new Promise((resolve, reject) => {
+        let data = "";
+        stream.on("data", chunk => (data += new TextDecoder().decode(chunk)));
+        stream.on("end", () => resolve(data));
+        stream.on("error", reject);
+    });
+}
 
 export async function TilbudMagiFunction(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
+
+    const readableStream = Readable.from(request.body);
+    const jsonTextBlob = await streamToString(readableStream);
+    const incomingData = JSON.parse(jsonTextBlob)
+
+    console.log("Received data: \n", incomingData)
 
     let ai_output = await PromptAI({model: 'gpt-4o-mini', role: 'user', instruction: 'Say hello'})
 
